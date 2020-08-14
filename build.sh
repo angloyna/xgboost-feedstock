003a8650de922e04b8788de71ec53dae0a79382e
Angela Gloyna <angela.gloyna@gmail.com>
2020-08-14 13:32:19 -0500

    try a binary repack

--- build.sh
+++ build.sh
@@ -3,24 +3,5 @@
 set -e
 set -x
 
-mkdir -p build
-cd build
-
-if [ ${cudatoolkit} == "8.0" ]; then
-    # CXXFLAGS is used to compile C code. -fvisibility-inlines-hidden
-    # is not valid for C which causes an issue when -Werror included
-    CXXFLAGS="${CXXFLAGS//-fvisibility-inlines-hidden/}"
-fi
-
-CXXFLAGS="-std=c++11"
-LDFLAGS=${LDFLAGS}" -L/usr/local/cuda/lib64"
-LIBRT=$(find ${BUILD_PREFIX} -name "librt.so")
-cmake \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DUSE_CUDA=ON \
-    -DCUDA_rt_LIBRARY=${LIBRT} \
-    -DCUDA_TOOLKIT_ROOT_DIR=${PREFIX} \
-    -DUSE_NCCL=ON \
-    -DNCCL_ROOT=/usr/local/cuda \
-    ..
-make -j${CPU_COUNT} ${VERBOSE_CM}
+mv xgboost-$PKG_VERSION-py3-none-any.whl.dummy xgboost-$PKG_VERSION-py3-none-any.whl
+$PYTHON -m pip install --no-deps xgboost-$PKG_VERSION-py3-none-any.whl
